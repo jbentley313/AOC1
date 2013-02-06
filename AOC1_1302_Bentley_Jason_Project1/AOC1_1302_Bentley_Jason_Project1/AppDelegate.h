9f31d8033db7000865b4eb9a1cbd0108b5ea36fd
Jason Bentley <jbxfade@fullsail.edu>
2013-02-05 22:03:52 -0800

    initial project 1 commit with working blank app

---
+++ AOC1_1302_Bentley_Jason_Project1/AOC1_1302_Bentley_Jason_Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1_1302_Bentley_Jason_Project1
+//
+//  Created by theOwner on 2/5/13.
+//  Copyright (c) 2013 theOwner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
