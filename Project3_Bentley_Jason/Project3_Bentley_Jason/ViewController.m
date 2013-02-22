d89ba28900630030ce2a3952b4dd282b744e4a70
Jason Bentley <jbxfade@fullsail.edu>
2013-02-21 17:21:03 -0800

    final week3 commit

--- Project3_Bentley_Jason/Project3_Bentley_Jason/ViewController.m
+++ Project3_Bentley_Jason/Project3_Bentley_Jason/ViewController.m
@@ -21,7 +21,7 @@
     
 }
 
-//Bool function
+//Compaer (Bool) function
 -(BOOL)Compare:(NSInteger)int1 int2:(NSInteger)int2
 {
     if (int2 == int1) {
@@ -43,7 +43,7 @@
 //DisplayAlertWithString Function
 -(void)DisplayAlertWithString:(NSString*)alert
 {
-    UIAlertView *alertViewMsg = [[UIAlertView alloc] initWithTitle:@"Hi!" message:alert delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    UIAlertView *alertViewMsg = [[UIAlertView alloc] initWithTitle:@"Alert!" message:alert delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
     if (alertViewMsg !=nil) {
         [alertViewMsg show];
     }
@@ -61,11 +61,9 @@
 {
     //Call Append and return new string
     NSString *message = [self Append:@"AOC1 1302 Project 3" string2:@" Jason Bentley"];
-    
-    
-    //Display the new string
-    [self DisplayAlertWithString:message];
-    
+        if (message !=nil) {
+            [self DisplayAlertWithString:message];
+        }
     
     //Add 2 NSIntegers and return
     int addedIntVal = [self Add:13 num2:42];
@@ -76,18 +74,23 @@
     //Convert NSNumber to NSString
     NSString *newString = [newNSNumber stringValue];
     
-
+    //String to be Appended
     NSString *newMsg = [NSString stringWithFormat:@"The number is "];
-    
-//    NSString *lastMsg = [self Add]
     
     //Show the sum of the two numbers within an alert
     NSString *newMessage = [self Append:newMsg string2:newString];
-    [self DisplayAlertWithString:newMessage];
+        if (newMessage !=nil) {
+            [self DisplayAlertWithString:newMessage];
+        }
     
-    
-    
-    
+    //Compare and display
+    NSInteger int1 = 24;
+    NSInteger int2 = 24;
+    BOOL compared = [self Compare:int1 int2:int2];
+    NSString *compareMsg = [NSString stringWithFormat:@"Are %d and %d the same integer? %@", int1, int2, compared? @"YES":@"NO"];
+        if (compared == YES) {
+            [self DisplayAlertWithString:compareMsg];
+        } else return;
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
